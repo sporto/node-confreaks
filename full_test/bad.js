972e6eab2f8ef666d858504cc58a3b5fc342fb1e
Sebastian <sebasporto@gmail.com>
2013-03-28 17:21:52 +1100

    return 404 instead of undefined

--- full_test/bad.js
+++ full_test/bad.js
@@ -3,3 +3,7 @@ main = require('../lib/main');
 main.scrape('http://www.confreaks.com/videos/222222244-erlangdc2013-cowboy-and-websockets', function (err, data) {
 	console.log(data);
 })
+
+main.scrape('http://www.confreaks.com/videos/', function (err, data) {
+	console.log(data);
+})
